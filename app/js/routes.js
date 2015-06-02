00995852a5e31a6bd0ff1b88a1e03cb965bd1948
Ginés Ortiz Sáez <gines.o.saez@gmail.com>
2015-06-02 13:32:42 +0200

    Text performance

--- app/js/routes.js
+++ app/js/routes.js
@@ -3,7 +3,7 @@
     define(['angular', 'app'], function (angular, app) {
         return app.config(['$routeProvider', '$locationProvider',
             function ($routeProvider, $locationProvider) {
-                //	$locationProvider.html5Mode(true);
+                //$locationProvider.html5Mode(true);
                 $routeProvider.when('/', {
                     templateUrl: 'app/features/home/views/home.html',
                     controller: 'homeCtrl'
